95485c3112de923c77a60d4663ee1ad0605c0bff
santty22 <144271939+santty22@users.noreply.github.com>
2024-09-09 04:46:51 -0300

    Errores corregidos (método)

---
+++ Trabajo Practico 1 - CT - Peter Santiago/practica tp 1/ArbolBinario.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Santy
+ * Fecha: 30/8/2024
+ * Hora: 00:57
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace trabajo_practico_1
+{
+	public class ArbolBinario<T>
+	{
+		private int dato;
+		private ArbolBinario<T> hijoIzquierdo;
+		private ArbolBinario<T> hijoDerecho;
+		
+		public int Dato {
+			get { return dato;}
+			set { dato = value;}
+		}
+		public ArbolBinario<T> HijoIzquierdo {
+			get { return hijoIzquierdo;}
+			set { hijoIzquierdo = value;}
+		}
+		public ArbolBinario<T> HijoDerecho {
+			get { return hijoDerecho;}
+			set { hijoDerecho = value;}
+		}
+		
+		public ArbolBinario(int dato) {
+			Dato = dato;
+			HijoIzquierdo = null;
+			HijoDerecho = null;
+		}
+		
+		// Métodos para agregar/eliminar hijos
+		public void agregarHijoIzquierdo(ArbolBinario<T> hijo) {
+			hijoIzquierdo = hijo;
+		}
+		public void agregarHijoDerecho(ArbolBinario<T> hijo) {
+			hijoDerecho = hijo;
+		}
+		public void eliminarHijoIzquierdo(ArbolBinario<T> hijo) {
+			hijoIzquierdo = null;
+		}
+		public void eliminarHijoDerecho(ArbolBinario<T> hijo) {
+			hijoDerecho = null;
+		}
+		
+		// Verificar si el nodo es hoja
+		public bool esHoja() {
+			return hijoIzquierdo == null && hijoDerecho == null;
+		}
+		
+		// Método para imprimir el árbol en preorden
+		public void ImprimirPreorden()
+		{
+			Console.WriteLine(Dato);
+			if (HijoIzquierdo!=null) {
+				HijoIzquierdo.ImprimirPreorden();
+			}
+			if (HijoDerecho!=null) {
+				HijoDerecho.ImprimirPreorden();
+			}
+		}
+		
+		public ArbolBinario<int> NuevoArbol(ArbolBinario<int> arbol)
+		{
+			// Si hay un hijo izquierdo, sumo el valor del padre con el hijo izquierdo
+			if (arbol.HijoIzquierdo != null) {
+				int nuevoDatoIzquierdo = arbol.Dato + arbol.HijoIzquierdo.Dato;
+				NuevoArbol(arbol.HijoIzquierdo);
+				arbol.HijoIzquierdo.Dato = nuevoDatoIzquierdo;
+			}
+			
+			// El hijo derecho del nuevo árbol es igual al del árbol original
+			if (arbol.HijoDerecho != null) {
+				NuevoArbol(arbol.HijoDerecho);
+			}
+			return arbol;
+		}
+	}
+}
---
+++ Trabajo Practico 1 - CT - Peter Santiago/practica tp 1/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Santy
+ * Fecha: 28/8/2024
+ * Hora: 21:36
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace trabajo_practico_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Números del ejercicio
+			ArbolBinario<int> arbol = new ArbolBinario<int>(1);
+			arbol.agregarHijoIzquierdo(new ArbolBinario<int>(2));
+			arbol.agregarHijoDerecho(new ArbolBinario<int>(3));
+			arbol.HijoIzquierdo.agregarHijoIzquierdo(new ArbolBinario<int>(4));
+			arbol.HijoDerecho.agregarHijoIzquierdo(new ArbolBinario<int>(5));
+			arbol.HijoDerecho.agregarHijoDerecho(new ArbolBinario<int>(6));
+			arbol.HijoDerecho.HijoIzquierdo.agregarHijoIzquierdo(new ArbolBinario<int>(7));
+			
+			Console.WriteLine("Árbol Original (Preorden):");
+			arbol.ImprimirPreorden();
+			
+			ArbolBinario<int> nuevoArbol = arbol.NuevoArbol(arbol);
+			
+			Console.WriteLine("Nuevo Árbol (Preorden):");
+			nuevoArbol.ImprimirPreorden();
+			
+			Console.ReadKey(true);
+		}
+	}
+}
